1828ff79706c789b8f5917062eb36961b7c2e411
bkj <ben@jataware.com>
2022-01-08 14:16:12 +0000

    model1

--- bkj_pretrain.py
+++ bkj_pretrain.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+
+"""
+    bkj_pretrain.py
+"""
+
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import normalize
@@ -33,8 +39,8 @@ def to_numpy(x):
 
 def parse_args():
     parser = argparse.ArgumentParser('MAE pre-training', add_help=False)
-    parser.add_argument('--batch_size', default=512, type=int)
-    parser.add_argument('--epochs',     default=400, type=int)
+    parser.add_argument('--batch_size', default=512,  type=int)
+    parser.add_argument('--epochs',     default=4000, type=int)
     
     parser.add_argument('--model',         default='mae_vit_base_patch16', type=str, metavar='MODEL')
     parser.add_argument('--input_size',    default=32, type=int)
@@ -42,15 +48,14 @@ def parse_args():
     parser.add_argument('--norm_pix_loss', action='store_true')
     parser.set_defaults(norm_pix_loss=False)
     
-    parser.add_argument('--weight_decay', type=float, default=0.05)
+    parser.add_argument('--weight_decay',  type=float, default=0.05)
     
-    parser.add_argument('--lr', type=float, default=1e-3, metavar='LR')
-    parser.add_argument('--min_lr', type=float, default=0., metavar='LR')
+    parser.add_argument('--lr',            type=float, default=2e-3)
+    parser.add_argument('--min_lr',        type=float, default=0.)
+    parser.add_argument('--warmup_epochs', type=int,   default=10)
     
-    parser.add_argument('--warmup_epochs', type=int, default=3, metavar='N')
-    
-    parser.add_argument('--output_dir', default='./output_dir')
-    parser.add_argument('--log_dir',    default='./output_dir')
+    parser.add_argument('--output_dir', default='./output_dir/run1')
+    parser.add_argument('--log_dir',    default='./output_dir/run1')
     parser.add_argument('--seed',       default=123, type=int)
     
     return parser.parse_args()
@@ -131,12 +136,11 @@ from torch import nn
 model = MaskedAutoencoderViT(
     img_size=32,
     patch_size=8,
-    # patch_size=16, 
-    embed_dim=256,
+    embed_dim=512,
     depth=8,
     num_heads=8,
     decoder_embed_dim=256,
-    decoder_depth=2,
+    decoder_depth=4,
     decoder_num_heads=8,
     mlp_ratio=4,
     norm_layer=partial(nn.LayerNorm, eps=1e-6)
@@ -148,18 +152,19 @@ model = model.to(device)
 param_groups = optim_factory.add_weight_decay(model, args.weight_decay)
 opt          = torch.optim.AdamW(param_groups, lr=args.lr, betas=(0.9, 0.95))
 
-lr = args.lr
-
 # --
 # Run
 
-for epoch in range(1600, 2000):
+for epoch in range(args.epochs):
     # --
     # Train
     
     _ = model.train()
     gen = tqdm(dl_train)
     for batch_idx, (x, _) in enumerate(gen):
+        
+        lr = lr_sched.adjust_learning_rate(opt, batch_idx / len(dl_train) + epoch, args)
+        
         x    = x.to(device, non_blocking=True)
         loss = model(x, mask_ratio=args.mask_ratio)
         
@@ -167,8 +172,8 @@ for epoch in range(1600, 2000):
         opt.step()
         opt.zero_grad()
         
-        log_writer.add_scalar('train_loss', float(loss), epoch * len(dl_train) + batch_idx)
-        log_writer.add_scalar('train_lr',   float(lr),   epoch * len(dl_train) + batch_idx)
+        log_writer.add_scalar('loss/train', float(loss), epoch * len(dl_train) + batch_idx)
+        log_writer.add_scalar('cfg/lr',   float(lr),   epoch * len(dl_train) + batch_idx)
         gen.set_postfix(loss=float(loss))
     
     # --
@@ -178,15 +183,20 @@ for epoch in range(1600, 2000):
         with torch.inference_mode():
             _ = model.eval()
             X_valid, y_valid = [], []
+            
+            loss = 0
             for batch_idx, (x, y) in enumerate(tqdm(dl_valid)):        
                 x   = x.to(device, non_blocking=True)
                 fts = model.forward_encoder(x, mask_ratio=0)[0]
                 
                 X_valid.append(to_numpy(fts.mean(axis=1)))
                 y_valid.append(to_numpy(y))
+                
+                loss += float(model(x, mask_ratio=args.mask_ratio))
             
             X_valid = normalize(np.row_stack(X_valid))
             y_valid = np.hstack(y_valid)
+            loss /= len(dl_valid)
             
             _ = model.eval()
             X_test, y_test = [], []
@@ -200,12 +210,18 @@ for epoch in range(1600, 2000):
             X_test = normalize(np.row_stack(X_test))
             y_test = np.hstack(y_test)
             
-            clf       = LinearSVC().fit(X_valid, y_valid)
+            if (epoch == 0) or (epoch % 100 != 0):
+                sel = np.random.choice(X_valid.shape[0], int(X_valid.shape[0] // 10), replace=False)
+            else:
+                sel = np.arange(X_valid.shape[0])
+            
+            clf       = LinearSVC().fit(X_valid[sel], y_valid[sel])
             valid_acc = (clf.predict(X_valid) == y_valid).mean()
             test_acc  = (clf.predict(X_test) == y_test).mean()
             
-            log_writer.add_scalar('valid_acc', float(valid_acc), epoch * len(dl_train))
-            log_writer.add_scalar('test_acc',  float(test_acc),  epoch * len(dl_train))
+            log_writer.add_scalar('acc/valid', float(valid_acc), epoch * len(dl_train))
+            log_writer.add_scalar('acc/test',  float(test_acc),  epoch * len(dl_train))
+            log_writer.add_scalar('loss/test', float(loss),  epoch * len(dl_train))
             log_writer.flush()
             print(valid_acc, test_acc)
 
@@ -214,4 +230,4 @@ for epoch in range(1600, 2000):
 # !! change model capacity
 
 state_dict = model.cpu().state_dict()
-torch.save(state_dict, 'cifar10_model0.pt')
+torch.save(state_dict, 'cifar10_model1.pt')
--- install.sh
+++ install.sh
@@ -8,20 +8,21 @@
 conda create -y -n mae_env python=3.8
 conda activate mae_env
 
-conda install -y -c pytorch \
-  pytorch torchvision cudatoolkit=11.3 
+# conda install -y -c pytorch \
+#   pytorch torchvision cudatoolkit=11.3
 
-conda install -y jupyter
-
+# A40
+pip3 install torch==1.10.1+cu113 \
+  torchvision==0.11.2+cu113 -f https://download.pytorch.org/whl/torch_stable.html
 
 pip install timm==0.3.2
-
 pip install git+https://github.com/bkj/rcode
-
 pip install tensorboard
 pip install requests
 pip install matplotlib
 pip install setuptools==59.5.0
+pip install tqdm
+pip install scikit-learn
 
 # --
 # Get models
